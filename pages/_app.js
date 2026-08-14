3075ce5b929b641ec03ff27b36b78cf661cb3188
cloakitxlo <cloakitxloa@pm.me>
2026-08-15 02:14:04 +0530

    chore: replace RainbowKit with direct wagmi connectors (Injected + WalletConnect)

--- pages/_app.js
+++ pages/_app.js
@@ -2,25 +2,32 @@ import '../styles/globals.css'
 import React from 'react'
 import { WagmiConfig, createClient, configureChains } from 'wagmi'
 import { publicProvider } from 'wagmi/providers/public'
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
-import '@rainbow-me/rainbowkit/styles.css'
 import { chain } from 'wagmi'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 
 const { chains, provider } = configureChains(
   [chain.mainnet, chain.polygon, chain.goerli],
   [publicProvider()]
 )
 
-const { connectors } = getDefaultWallets({ appName: 'Cardexah Clone', chains })
+function getConnectors() {
+  const injected = new InjectedConnector({ chains })
+  const walletConnect = new WalletConnectConnector({
+    chains,
+    options: {
+      projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || ''
+    }
+  })
+  return [injected, walletConnect]
+}
 
-const wagmiClient = createClient({ autoConnect: true, connectors, provider })
+const wagmiClient = createClient({ autoConnect: true, connectors: getConnectors(), provider })
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
-        <Component {...pageProps} />
-      </RainbowKitProvider>
+      <Component {...pageProps} />
     </WagmiConfig>
   )
 }
